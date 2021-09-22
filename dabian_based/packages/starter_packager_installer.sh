45b5920d514bafa84598121c4eee2a287f54b4e0
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2021-09-23 00:27:59 +0530

    Create starter_packager_installer.sh

---
+++ dabian_based/packages/starter_packager_installer.sh
@@ -0,0 +1,18 @@
+echo "\u001b[36m welcome to starter package installer \u001b[0m"
+echo "\u001b[33m Installing - \u001b[0m"
+echo "Brave-brouser."
+echo "git"
+echo "gedit"
+echo "visual studio code"
+echo -n "\u001b[31m Are you sure to go forward ? [y/n]: \u001b[0m"
+read perm
+echo -n "\u001b[36m want to set up github ssh connection? [y/n]"
+read perm
+if [ perm == "y"]
+then
+echo -n "\u001b[36m please enter email address : "
+read email
+echo -n "\u001b[33m please enter fullname address : "
+read name
+
+    
